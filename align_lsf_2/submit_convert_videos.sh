f73c863b81b0749adfeb5817b01bd9b228911d9b
Mathias Müller <mathias.mueller@uzh.ch>
2022-10-28 15:15:26 +0200

    prepare align_lsf_2

--- align_lsf_2/submit_convert_videos.sh
+++ align_lsf_2/submit_convert_videos.sh
@@ -5,6 +5,10 @@ module load generic anaconda3
 base=$(dirname "$0")/..
 base=$(realpath $base)
 
+# sets variable: file_ids
+
+. $base/align_lsf_2/define_ids.sh
+
 logs=$base/align_lsf_2/logs
 logs_sub=$logs/convert_videos
 
@@ -14,10 +18,12 @@ mkdir -p $logs_sub
 SLURM_DEFAULT_FILE_PATTERN="slurm-%j.out"
 SLURM_LOG_ARGS="-o $logs_sub/$SLURM_DEFAULT_FILE_PATTERN -e $logs_sub/$SLURM_DEFAULT_FILE_PATTERN"
 
-SLURM_ARGS_GENERIC="--cpus-per-task=8 --time=24:00:00 --mem=8G --partition=generic"
+SLURM_ARGS_GENERIC="--cpus-per-task=8 --time=01:00:00 --mem=8G --partition=generic"
 
-sbatch \
-    $SLURM_ARGS_GENERIC \
-    $SLURM_LOG_ARGS \
-    $base/align_lsf_2/convert_videos.sh \
-    $base
+for file_id in $file_ids; do
+    sbatch \
+        $SLURM_ARGS_GENERIC \
+        $SLURM_LOG_ARGS \
+        $base/align_lsf_2/convert_video.sh \
+        $base $file_id
+done

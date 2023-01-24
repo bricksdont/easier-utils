b43acdf6da0df5132cf4fdeddd59de9465a22b5e
Mathias Müller <mathias.mueller@uzh.ch>
2023-01-24 16:35:19 +0100

    update file ids

--- align_lis/convert_video.sh
+++ align_lis/convert_video.sh
@@ -5,12 +5,14 @@ file_id=$2
 
 data=/scratch/mathmu/align_lis
 
+prefix="rsi"
+
 eval "$(conda shell.bash hook)"
 source activate /shares/easier.volk.cl.uzh/WMT_Shared_Task/processing-shared-task-data/venvs/venv
 
 mkdir -p $data/videos_mp4
 
 python $base/convert_ts_to_mp4.py \
-   --input $data/videos_ts/rts.$file_id.ts \
-   --output $data/videos_mp4/rts.$file_id.mp4 \
+   --input $data/videos_ts/$prefix.$file_id.ts \
+   --output $data/videos_mp4/$prefix.$file_id.mp4 \
    --ffmpeg-multithreaded

f289d23c1c8a59d2c020818f707f585f8391aefd
Mathias Müller <mathias.mueller@uzh.ch>
2023-07-06 10:13:42 +0200

    find ambiguous filenames

---
+++ find_ambiguous_filenames.py
@@ -0,0 +1,78 @@
+#! /usr/bin/python3
+
+"""
+Find duplicate IDs in filenames
+"""
+
+import os
+import argparse
+import logging
+
+from collections import Counter
+from typing import List
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--folder", type=str,
+                        help="Folder with files.", required=True)
+
+    parser.add_argument("--ignores", type=str, nargs="+",
+                        help="Filename components to ignore.", required=False)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def get_ambiguous_elements(filenames: List[str]) -> List[str]:
+    """
+
+    :param filenames:
+    :return:
+    """
+
+    return [k for k, v in Counter(filenames).items() if v > 1]
+
+
+def remove_ignore_components(filenames: List[str], ignores: List[str]) -> List[str]:
+    """
+
+    :param filenames:
+    :param ignores:
+    :return:
+    """
+    new_filenames = []
+
+    for filename in filenames:
+        new_filename = filename
+        for ignore in ignores:
+            new_filename = new_filename.replace(ignore, "")
+        new_filename = new_filename.replace(".", "")
+
+        new_filenames.append(new_filename)
+
+    return new_filenames
+
+
+def main():
+
+    args = parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    logging.debug(args)
+
+    filenames = list(os.listdir(args.folder))
+
+    filenames_ignored = remove_ignore_components(filenames, args.ignores)
+
+    ambiguous_filenames = get_ambiguous_elements(filenames_ignored)
+
+    print(ambiguous_filenames)
+    print("Total number of ambiguous filenames: %d" % len(ambiguous_filenames))
+
+
+if __name__ == '__main__':
+    main()
